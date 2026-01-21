a11fb23fad17d2914bb471432f277f326f2046bc
Himanshu <mauryah376@gmail.com>
2026-01-21 23:56:30 +0530

    authentication implementation

--- BACKEND/src/controller/auth.controller.js
+++ BACKEND/src/controller/auth.controller.js
@@ -4,13 +4,13 @@ import wrapAsync from "../utils/tryCatchWrapper.js"
 
 export const register_user =wrapAsync(async(req,res)=>{
    const {name,email,password}= req.body 
-   const token= await registerUser({name,email,password})
+   const token= await registerUser(name,email,password)
    res.cookie("accesToken",token,cookieOptions)
    res.status(200).json({message:"Login Success"})
 })
 export const login_user = wrapAsync(async(req,res)=>{
     const {email,password}= req.body
-    const token=  loginUser({email,password})
+    const token= await  loginUser(email,password)
 
     res.cookie("accesToken",token,cookieOptions)
     res.status(200).json({message:"Suceesfully logged in"})
