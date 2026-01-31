2f3ee27cfda64b7198b0f32c34bdc5490339abbe
Himanshu <mauryah376@gmail.com>
2026-02-01 00:40:03 +0530

    backend error fix

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express"
 const app= express();
 import dotenv, { configDotenv } from "dotenv"
@@ -17,37 +16,9 @@ app.use(cors({
     origin: `${process.env.Client_URL}` || "http://localhost:5173",
     credentials:true
 }))
-dotenv.config()
-=======
-// import express from "express"
-// const app= express();
-// import { nanoid } from "nanoid"; 
-// import dotenv, { configDotenv } from "dotenv"
-// configDotenv();
-// import connectDB from "./src/config/mongo.config.js";
-// import urlSchema from "./src/models/short_url.model.js";
-// import short_url from "./src/routes/short_url.route.js"
-// import user_routes from "./src/routes/user.routes.js"
-// import auth_routes from "./src/routes/auth.routes.js"
-// import { redirectFromShortUrl } from "./src/controller/short_url.controller.js";
-// import { errorHandler } from "./src/utils/ErrorHandler.js";
-// import cors from "cors"
-// import { attachUser } from "./src/utils/attachUser.js";
-// import cookieParser from "cookie-parser";
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-// // console.log(process.env.Client_URL,"==========")
-
-// app.use(cors({
-//     origin: `${process.env.Client_URL}` || "http://localhost:5173",
-//     credentials:true
-// }))
-// dotenv.config()
->>>>>>> 78d5f52de1c81bafc8987111e8b278c047f3f580
-
-// app.use(express.json())
-// app.use(express.urlencoded({extended:true}))
-
-<<<<<<< HEAD
 app.use(cookieParser())
 app.use(attachUser)
 app.use("/api/user",user_routes)
@@ -55,20 +26,7 @@ app.use("/api/create",short_url)
 app.use("/api/auth",auth_routes)
 app.get("/:id", redirectFromShortUrl)
 app.use(errorHandler)    
-=======
-// app.use(cookieParser())
-// app.use(attachUser)
-// app.use("/api/user",user_routes)
-// app.use("/api/create",short_url)
-// app.use("/api/auth",auth_routes)
-// app.get("/:id", redirectFromShortUrl)
-// app.use(errorHandler)   
-
-// app.listen(3000,()=>{
-//     connectDB()
-//     console.log("Server is running on port on http://localhost:3000")
-// })
-
-// //get - Redirection 
-// //post - create short url 
->>>>>>> 78d5f52de1c81bafc8987111e8b278c047f3f580
+connectDB()
+app.listen(3000,()=>{
+    console.log("Server connected")
+})
